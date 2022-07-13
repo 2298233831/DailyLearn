315bbceaa11a01de2f8b90f4d66de9e38a27a46a
2298233831 <90432564+2298233831@users.noreply.github.com>
2022-07-13 08:54:54 +0800

    Add files via upload

---
+++ IAT hook.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <Windows.h>
+
+
+int YourMessage(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
+int hookiat();
+
+int main()
+{
+	MessageBoxA(NULL, "Hello World", "hhh", 0);
+	hookiat();
+	MessageBoxA(NULL, "Hello World", "hhh", 0);
+    return 0;
+}
+
+
+int YourMessage(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType)
+{
+	MessageBoxW(NULL, L"Hello IAT Hook", L"The IAT Have been hooked", 0);
+	return 0;
+}
+
+int hookiat()
+{
+	PVOID imageBase = GetModuleHandleA(NULL);
+	PIMAGE_DOS_HEADER dosHeaders = (PIMAGE_DOS_HEADER)imageBase;
+	PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)((DWORD_PTR)imageBase + dosHeaders->e_lfanew);
+
+	PIMAGE_IMPORT_DESCRIPTOR importDescriptor = NULL;
+	IMAGE_DATA_DIRECTORY importsDirectory = ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+	importDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)(importsDirectory.VirtualAddress + (DWORD_PTR)imageBase);
+	LPCSTR DllName = NULL;
+	HMODULE library = NULL;
+	PIMAGE_IMPORT_BY_NAME functionName = NULL;
+
+	while (importDescriptor->Name != NULL)
+	{
+		DllName = (LPCSTR)importDescriptor->Name + (DWORD_PTR)imageBase;
+		library = LoadLibraryA(DllName);
+
+		if (library)
+		{
+			PIMAGE_THUNK_DATA PINT = NULL, PIAT = NULL;
+			PINT = (PIMAGE_THUNK_DATA)((DWORD_PTR)imageBase + importDescriptor->OriginalFirstThunk);
+			PIAT = (PIMAGE_THUNK_DATA)((DWORD_PTR)imageBase + importDescriptor->FirstThunk);
+
+			while (PINT->u1.AddressOfData != NULL)
+			{
+				functionName = (PIMAGE_IMPORT_BY_NAME)((DWORD_PTR)imageBase + PINT->u1.AddressOfData);
+				if (!strcmp(functionName->Name, "MessageBoxA"))
+				{
+
+					DWORD oldProtect = 0;
+					VirtualProtect((LPVOID)(&PIAT->u1.Function), 8, PAGE_READWRITE, &oldProtect);
+					PIAT->u1.Function = (DWORD_PTR)YourMessage;
+					VirtualProtect((LPVOID)(&PIAT->u1.Function), 4, oldProtect, &oldProtect);
+				}
+				++PINT;
+				++PIAT;
+			}
+		}
+		importDescriptor++;
+	}
+	
+
+	return 0;
+}
